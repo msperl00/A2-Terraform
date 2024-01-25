c139f4a0f18a96bc9e87c2177081bd969aa88f13
Marco <msperl00@estudiantes.unileon.es>
2024-01-26 00:25:44 +0100

    add v1.0 with ssh

--- workspace/main.tf
+++ workspace/main.tf
@@ -1,51 +1,78 @@
 terraform {
-    #Necesario para el terraform
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-  required_version = ">= 1.7.0"
+  required_version = ">= 1.5.0"
 }
 
 # Configura el proveedor AWS utilizando el perfil configurado en la AWS CLI
 provider "aws" {
-  region = "eu-south-2"
+  region = "us-east-1"
 }
 
-# Crea una VPC para alojar tus recursos
+# Crear VPC
 resource "aws_vpc" "mean_vpc" {
-  cidr_block = "10.0.0.0/16" # Este es el rango de direcciones IP para tu VPC.
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "mean_vpc"
   }
 }
 
-# Define las subredes dentro de la VPC
+# Crear Subred
 resource "aws_subnet" "mean_subnet" {
-  vpc_id     = aws_vpc.mean_vpc.id # Asocia esta subred con la VPC que acabas de definir.
-  cidr_block = "10.0.1.0/24" # Subconjunto del bloque CIDR de la VPC para esta subred.
+  vpc_id     = aws_vpc.mean_vpc.id
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "mean_subnet"
   }
 }
 
-# Crea un grupo de seguridad para tus instancias EC2
+# Crear Internet Gateway
+resource "aws_internet_gateway" "mean_gateway" {
+  vpc_id = aws_vpc.mean_vpc.id
+
+  tags = {
+    Name = "mean_gateway"
+  }
+}
+
+# Crear Tabla de Ruteo
+resource "aws_route_table" "mean_route_table" {
+  vpc_id = aws_vpc.mean_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mean_gateway.id
+  }
+
+  tags = {
+    Name = "mean_route_table"
+  }
+}
+
+
+# Asociar la Tabla de Ruteo con la Subred
+resource "aws_route_table_association" "mean_route_table_assoc" {
+  subnet_id      = aws_subnet.mean_subnet.id
+  route_table_id = aws_route_table.mean_route_table.id
+}
 resource "aws_security_group" "mean_sg" {
   name        = "mean_security_group"
   description = "Security group for MEAN stack"
-  vpc_id      = aws_vpc.mean_vpc.id
-
-  # Regla de ingreso: permite el tráfico entrante HTTP y SSH
+  vpc_id      = aws_vpc.mean_vpc.id  
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -53,15 +80,35 @@ resource "aws_security_group" "mean_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["212.183.248.241/32"] # Lo reemplazo con mi IP pública para acceso SSH.
+    #cidr_blocks = ["212.183.248.241/32"] # Mi conexión
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
-  # Regla de egreso: permite todo el tráfico saliente
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["212.183.248.241/32"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Regla para MongoDB
+  ingress {
+    description      = "MongoDB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  # Reglas de salida para permitir todas las conexiones salientes
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Permite todas
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -69,41 +116,56 @@ resource "aws_security_group" "mean_sg" {
   }
 }
 
+# Cargar la clave pública SSH en AWS
+resource "aws_key_pair" "keypair" {
+  key_name   = "mi_clave_ssh"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "mi_clave_ssh" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "clave"
+}
+
 # Instancia EC2 para Node.js
+# Asegúrate de actualizar las instancias EC2 para usar la subred y el grupo de seguridad VPC
 resource "aws_instance" "node_instance" {
-  ami           = "ami-0a9e7160cebfd8c12" 
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.mean_subnet.id
-  security_groups = [aws_security_group.mean_sg.name]
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.mean_subnet.id
+  vpc_security_group_ids = [aws_security_group.mean_sg.id]
+  private_ip             = "10.0.1.11"
+  key_name               = "mi_clave_ssh"
+  associate_public_ip_address = true
 
   tags = {
     Name = "NodeInstance"
   }
 }
-
 # Instancia EC2 para MongoDB
 resource "aws_instance" "mongo_instance" {
-  ami           = "ami-04724c78a5bbb23bb" 
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.mean_subnet.id
-  security_groups = [aws_security_group.mean_sg.name]
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.mean_subnet.id
+  vpc_security_group_ids = [aws_security_group.mean_sg.id]
+  private_ip             = "10.0.1.10"
+  key_name               = "mi_clave_ssh"
+  associate_public_ip_address = true
 
   tags = {
     Name = "MongoInstance"
   }
 }
 
-# Salida: IPs públicas de las instancias
 output "node_instance_ip" {
   value = aws_instance.node_instance.public_ip
-  description = "IP pública de la instancia Node.js"
 }
 
 output "mongo_instance_ip" {
   value = aws_instance.mongo_instance.public_ip
-  description = "IP pública de la instancia MongoDB"
-}
-
-output "mongodb_ip" {
-  value = aws_instance.mongodb.public_ip
 }
